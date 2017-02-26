b2f8e03330c136a944e308b8c93ef70d695f3705
alexiasa <alexiasa@users.noreply.github.com>
2017-02-25 23:45:44 -0800

    initial commit

---
+++ src/Anagram.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ */
+public class Anagram {
+    private String key;
+    private ArrayList<String> values = new ArrayList<>();
+
+    /**
+     * @param newWord the default Anagram constructor requires a new word String. This new word will be the first entry
+     *                in values. The key is computed from this word.
+     */
+    public Anagram(String newWord) {
+        this.key = computeKey(newWord);
+        this.values.add(newWord);
+
+    }
+
+    /**
+     * @param word will be a value of the Anagram. We sort this alphabetically to find the key
+     * @return the computed key (sorted alphabetically) of the word as a string
+     */
+    public static String computeKey(String word) {
+        char[] wordChars = word.toCharArray();
+        Arrays.sort(wordChars);
+        return new String(wordChars);
+    }
+
+    /**
+     * @return this.values
+     */
+    public ArrayList<String> getValues() {
+        return this.values;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    /**
+     * accepts a word and adds it to the Anagram object's values
+     * @param anagramWord is an original word that contains the same letters as the Anagram object's key
+     */
+    public void setValues(String anagramWord) {
+        this.values.add(anagramWord);
+    }
+}
---
+++ tests/TestAnagramClass.java
@@ -0,0 +1,75 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ */
+public class TestAnagramClass {
+
+    private Anagram setUp(String testWord) {
+        return new Anagram(testWord);
+    }
+
+    @Test
+    void testComputeKey() {
+        String actual = Anagram.computeKey("range");
+        String expected = "aegnr";
+        Assertions.assertEquals(expected, actual);
+        System.out.printf("Expected %s, got %s", expected, actual);
+        System.out.println();
+
+    }
+
+    @Test
+    void testGetValues() {
+        Anagram testGram = setUp("range");
+
+        String expected = "[range]";
+        String actual = testGram.getValues().toString();
+        Assertions.assertEquals(expected, actual);
+        System.out.printf("Expected %s, got %s", expected, actual);
+        System.out.println();
+
+    }
+
+    @Test
+    void testSetValues() {
+        Anagram testGram = setUp("range");
+
+        String testWord = "anger";
+        String expected = "[range, anger]";
+        testGram.setValues(testWord);
+
+        String actual = testGram.getValues().toString();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAnagramDefault() {
+        Anagram testGram = setUp("range");
+        String expectedKey = "aegnr";
+        String expectedValue = "[range]";
+
+        Assertions.assertEquals(expectedKey, testGram.getKey());
+        Assertions.assertEquals(expectedValue, testGram.getValues().toString());
+
+        System.out.println(testGram.getValues());
+        System.out.println(testGram.getKey());
+
+    }
+
+    @Test
+    void testGetKey() {
+        Anagram testGram = setUp("range");
+        String expectedKey = Anagram.computeKey("range");
+        String actualKey = testGram.getKey();
+
+        Assertions.assertEquals(expectedKey, actualKey);
+
+
+
+
+    }
+
+}
